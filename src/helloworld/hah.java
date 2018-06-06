74feffd0d3399ed56b5395224eddb37a347f8bce
maifayan <987872609@qq.com>
2018-06-06 11:00:33 +0800

    test2

---
+++ src/helloworld/hah.java
@@ -0,0 +1,11 @@
+package helloworld;
+/**
+ * 
+ * @author WQHJD
+ *
+ */
+public class hah {
+   public void wanne() {
+	System.out.println("玩你妹啊");
+}
+}
--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World");
+        System.out.println("Hello World 我想改改，可以吗，不可以吗");
 	}
 
 }
